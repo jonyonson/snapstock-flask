ec575785d8a2e9fb2ef116b6c2b7f87e17d01a0c
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-06-26 17:47:30 -0500

    Revert "remove unused route to individual index"
    This reverts commit 5498dff6e2ed60a6409a9115a89d55a5f33416c0.

--- app.py
+++ app.py
@@ -26,6 +26,35 @@ def indices():
     return jsonify(major_indices), 200
 
 
+@app.route('/indices/<index>')
+def get_index(index):
+    if index == 'dow':
+        dow = get_quote_table('^dji')
+        dow['Change'] = dow['Quote Price'] - dow['Previous Close']
+        dow['Percent Change'] = dow['Change'] / dow['Previous Close'] * 100
+        result = dow
+    elif index == 'sp500':
+        sp500 = get_quote_table('^gspc')
+        sp500['Change'] = sp500['Quote Price'] - sp500['Previous Close']
+        sp500['Percent Change'] = sp500['Change'] / \
+            sp500['Previous Close'] * 100
+        result = sp500
+    elif index == 'nasdaq':
+        nasdaq = get_quote_table('^ixic')
+        nasdaq['Change'] = nasdaq['Quote Price'] - nasdaq['Previous Close']
+        nasdaq['Percent Change'] = nasdaq['Change'] / \
+            nasdaq['Previous Close'] * 100
+        result = nasdaq
+    elif index == 'russell':
+        russell = get_quote_table('^gspc')
+        russell['Change'] = russell['Quote Price'] - russell['Previous Close']
+        russell['Percent Change'] = russell['Change'] / \
+            russell['Previous Close'] * 100
+        result = russell
+
+    return jsonify({index: result}), 200
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 4000))
     app.run(host='0.0.0.0', port=port)
